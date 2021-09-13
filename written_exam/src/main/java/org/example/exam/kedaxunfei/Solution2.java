ca4dd7e1ef3488a715d95eac214b8a85e31f8435
lifei <1058235935@qq.com>
2021-09-13 12:03:54 +0800

    科大讯飞第二题

--- written_exam/src/main/java/org/example/exam/kedaxunfei/Solution2.java
+++ written_exam/src/main/java/org/example/exam/kedaxunfei/Solution2.java
@@ -1,9 +1,11 @@
 package org.example.exam.kedaxunfei;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
- * 题目描述：
+ * 题目描述：leetcode 611 变体。
  * 输入：[6,6,6]
  * 输出：0
  * 说明：
@@ -19,24 +21,56 @@ public class Solution2 {
     public int happyTriangle(int[] length) {
         int n = length.length;
         Arrays.sort(length);
-        Set<Integer> val = new HashSet<>(n);
-        Set<Integer> square = new HashSet<>(n);
-        Map<Integer, Integer> map = new HashMap<>(n);
-        for (int i = 0; i < n; i++) {
-            val.add(length[i]);
-            square.add(length[i] * length[i]);
+        System.out.println(Arrays.toString(length));
+        // 每个元素有多少个
+        int[] valToCount = new int[2001];
+        // 每个元素的平方，快速判断平方是否存在
+        Set<Integer> square = new HashSet<>();
+        for (int cur : length) {
+            valToCount[cur] += 1;
+            square.add(cur * cur);
         }
-        for (int i = 0; i < n - 2; i++) {
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
             int a = length[i];
-            for (int j = i + 1; j < n - 1; j++) {
+            for (int j = i + 1; j < n; j++) {
                 int b = length[j];
+                int left = j + 1;
+                int right = n - 1;
+                int k = j;
+                while (left <= right) {
+                    int mid = (left + right) >> 1;
+                    if (length[mid] < a + b) {
+                        k = mid;
+                        left = mid + 1;
+                    } else {
+                        right = mid - 1;
+                    }
+                }
+                sum += (k - j);
+                // 排除等边三角形
+                if (a == b) {
+                    left = j + 1;
+                    while (left < n && length[left] == b) {
+                        left++;
+                    }
+                    sum -= (left - j - 1);
+                }
+                // 排除直角三角形
+                int cs = a * a + b * b;
+                if (square.contains(cs)) {
+                    int c = (int) Math.sqrt(cs);
+                    sum -= valToCount[c];
+                }
+                System.out.println("i = " + i + ", j = " + j + ", sum = " + sum);
             }
         }
-        return 0;
+        return sum;
     }
 
     public static void main(String[] args) {
         int[] length = new int[]{5, 3, 4, 6, 6, 1};
+        // int[] length = new int[]{2, 2, 2, 3};
         System.out.println(new Solution2().happyTriangle(length));
     }
 }
