777e1a1e4ebf7c92446cf5192ff4fddda6a2530b
lifei <1058235935@qq.com>
2021-09-15 10:29:05 +0800

    用栈实现队列、用队列实现栈

---
+++ algo-algorithm/src/main/java/org/example/algo/custom/Solution225.java
@@ -0,0 +1,50 @@
+package org.example.algo.custom;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * https://leetcode-cn.com/problems/implement-stack-using-queues/
+ * <p>
+ * 简单
+ *
+ * @author lifei
+ */
+public class Solution225 {
+
+    class MyStack {
+        Queue<Integer> queue;
+        int topElem = 0;
+
+        public MyStack() {
+            this.queue = new LinkedList<>();
+        }
+
+        public void push(int x) {
+            queue.offer(x);
+            topElem = x;
+        }
+
+        public int pop() {
+            int size = queue.size();
+            while (size > 2) {
+                queue.offer(queue.poll());
+                size--;
+            }
+            topElem = queue.peek();
+            queue.offer(queue.poll());
+            return queue.poll();
+        }
+
+        public int top() {
+            return topElem;
+        }
+
+        public boolean empty() {
+            return queue.isEmpty();
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
---
+++ algo-algorithm/src/main/java/org/example/algo/custom/Solution232.java
@@ -0,0 +1,49 @@
+package org.example.algo.custom;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * https://leetcode-cn.com/problems/implement-queue-using-stacks/
+ * <p>
+ * 简单
+ *
+ * @author lifei
+ */
+public class Solution232 {
+
+    class MyQueue {
+        Deque<Integer> inStack;
+        Deque<Integer> outStack;
+
+        public MyQueue() {
+            this.inStack = new LinkedList<>();
+            this.outStack = new LinkedList<>();
+        }
+
+        public void push(int x) {
+            inStack.push(x);
+        }
+
+        public int pop() {
+            peek();
+            return outStack.pop();
+        }
+
+        public int peek() {
+            if (outStack.isEmpty()) {
+                while (!inStack.isEmpty()) {
+                    outStack.push(inStack.pop());
+                }
+            }
+            return outStack.peek();
+        }
+
+        public boolean empty() {
+            return inStack.isEmpty() && outStack.isEmpty();
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
